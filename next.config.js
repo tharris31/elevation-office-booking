535340ce6f271318df31516600d7c32c4b708b1b
tharris31 <tharris@elevationtherapy.org>
2025-09-24 16:33:12 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      { source: '/therapist', destination: '/therapists', permanent: true },
+    ];
+  },
+};
 module.exports = nextConfig;

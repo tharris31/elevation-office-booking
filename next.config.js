4dc5ba9243a5cb95b5a9b670b95c4161442c5895
tharris31 <tharris@elevationtherapy.org>
2025-10-01 15:24:47 -0500

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      { source: '/therapist', destination: '/therapists', permanent: true },
-    ];
-  },
-};
-module.exports = nextConfig;
